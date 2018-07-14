2c6bb11b3d63a48797bb5ce5026b94a0b2bc600c
wangabeng <wangabeng@163.com>
2018-07-14 15:20:00 +0800

    add

--- componentsPreloader/preloader.js
+++ componentsPreloader/preloader.js
@@ -1,20 +1,36 @@
+/*
+    * 图片预加载
+ */
+
 !function ($) {
     // 1 定义构造函数
     var PicPreloader = function (el) {
-        this.el = el;
+        this.el = el.find('img');
+        console.log(this.el);
+        this.realSrc = '';
+        this.initPic();
         this.reloadPic();
     };
     // 2 定义原型方法
     PicPreloader.prototype = {
+        initPic: function () {
+            this.el.each(function (index, element) {
+                var realSrc = $(element).attr("src");
+                $(element).attr("src", "./img/test.png");
+                $(element).attr("data-src", realSrc);
+            });
+        },
         // 预加载后替换图片
         reloadPic: function () {
-            console.log(this.el);
+            // console.log(this.el);
             var _this = this;
-            this.imageObj = new Image();
-            this.imageObj.src = this.el.attr('data-src');
-            this.imageObj.onload = function () {
-                _this.el.attr('src', _this.imageObj.src);
-            };
+            this.el.each(function (index, element) {
+                var newImageObj = new Image();
+                newImageObj.src = $(element).attr('data-src');
+                newImageObj.onload = function () {
+                    $(element).attr('src', newImageObj.src);
+                };
+            });
         }
     };
     // 3 拓展jquery函数

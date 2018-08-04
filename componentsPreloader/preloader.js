f66c48dc36622ea9bdee0cd3796a9576c24e148f
wangabeng <wangabeng@163.com>
2018-08-04 11:15:35 +0800

    add

--- componentsPreloader/preloader.js
+++ componentsPreloader/preloader.js
@@ -1,12 +1,21 @@
 /*
-    * 图片预加载
+    图片预加载插件
+
  */
+
 
 !function ($) {
     // 1 定义构造函数
-    var PicPreloader = function (el) {
-        this.el = el.find('img');
-        console.log(this.el);
+    var PicPreloader = function (el, imgUrl) {
+        // 如果自身是IMG元素 就不再找容器内的IMG元素
+        if (el[0].tagName == 'IMG') {
+            this.el = el;
+        } else {
+            this.el = el.find('img');
+        }
+        this.imgUrl = imgUrl;
+
+        // console.log(this.el);
         this.realSrc = '';
         this.initPic();
         this.reloadPic();
@@ -14,9 +23,10 @@
     // 2 定义原型方法
     PicPreloader.prototype = {
         initPic: function () {
+            var _this = this;
             this.el.each(function (index, element) {
                 var realSrc = $(element).attr("src");
-                $(element).attr("src", "./img/test.png");
+                $(element).attr("src", _this.imgUrl);
                 $(element).attr("data-src", realSrc);
             });
         },
@@ -35,9 +45,9 @@
     };
     // 3 拓展jquery函数
     $.fn.extend({
-        picPreloader: function () {
+        picPreloader: function (imgUrl) {
             this.each(function(){
-                new PicPreloader($(this));
+                new PicPreloader($(this), imgUrl);
             });
             return $(this);
         }
